f40f8111893af5ab09f846cb2b167fb6c98df5be
2021012887 <l5TYaPWY8STx>
2023-12-17 17:02:17 +0800

    start new app

--- user_auth/urls.py
+++ user_auth/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('register', views.register, name='register'),
     path('login', views.login_view, name='login'),
     path('logout', views.logout_view, name='logout'),
-    path('user_info', views.user_info, name='user_info'),
+    path('userinfo', views.user_info, name='user_info'),
 ]
