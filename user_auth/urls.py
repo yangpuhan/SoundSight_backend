43400e253217fea02849ecdc6b00f327bdad3854
2021012887 <l5TYaPWY8STx>
2023-12-17 16:47:05 +0800

    adjust

--- user_auth/urls.py
+++ user_auth/urls.py
@@ -7,7 +7,4 @@ urlpatterns = [
     path('login', views.login_view, name='login'),
     path('logout', views.logout_view, name='logout'),
     path('user_info', views.user_info, name='user_info'),
-    path('realtime/begin', views.realtime_begin, name='realtime_begin'),
-    path('realtime/middle', views.realtime_middle, name='realtime_middle'),
-    path('realtime/end', views.realtime_end, name='realtime_end'),
 ]
